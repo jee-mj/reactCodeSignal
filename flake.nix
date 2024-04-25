d31c3f41f5db6bf30188733b7ac40e0e84e2ae42
jee-mj <28581723+jee-mj@users.noreply.github.com>
2024-04-26 07:06:50 +1000

    init

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+          ];
+          shellHook = ''
+            echo "Node.js development environment is ready."
+          '';
+        };
+      }
+    );
+}
